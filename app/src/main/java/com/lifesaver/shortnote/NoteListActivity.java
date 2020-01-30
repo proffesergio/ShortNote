02d12662ceef95e67e1822ca7e90040f93239e09
null <bilal.hossai1@gmail.com>
2020-01-30 15:21:54 +0600

    Custom Toolbar added with scrollable behavior functionality

--- app/src/main/java/com/lifesaver/shortnote/NoteListActivity.java
+++ app/src/main/java/com/lifesaver/shortnote/NoteListActivity.java
@@ -1,6 +1,7 @@
 package com.lifesaver.shortnote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -33,6 +34,9 @@ public class NoteListActivity extends AppCompatActivity {
         initRecyclerView();
 
         insertNotes();
+
+        Toolbar mToolbar = ((Toolbar) findViewById(R.id.notes_toolbar));
+        mToolbar.setTitle("Short Notes");
     }
 
     private void insertNotes() {
