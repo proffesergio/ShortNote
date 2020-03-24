441964cbd3c746f327c42e6d4e2684f68970448d
null <bilal.hossai1@gmail.com>
2020-03-24 23:08:21 +0600

    Floating Action Button Added

--- app/src/main/java/com/lifesaver/shortnote/NoteListActivity.java
+++ app/src/main/java/com/lifesaver/shortnote/NoteListActivity.java
@@ -9,14 +9,17 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.lifesaver.shortnote.adapters.NotesRecyclerAdapter;
 import com.lifesaver.shortnote.data_models.Note;
 import com.lifesaver.shortnote.utils.VerticalSpacingItemDecorator;
 
 import java.util.ArrayList;
 
-public class NoteListActivity extends AppCompatActivity implements NotesRecyclerAdapter.OnNoteListener {
+public class NoteListActivity extends AppCompatActivity implements NotesRecyclerAdapter.OnNoteListener,
+        View.OnClickListener {
 
     //Ui Components
     private RecyclerView mRecyclerView;
@@ -32,6 +35,7 @@ public class NoteListActivity extends AppCompatActivity implements NotesRecycler
         setContentView(R.layout.activity_note_list);
 
         mRecyclerView = findViewById(R.id.recyclerView);
+        findViewById(R.id.add_note_fab).setOnClickListener(this);
 
         initRecyclerView();
 
@@ -75,6 +79,13 @@ public class NoteListActivity extends AppCompatActivity implements NotesRecycler
 
         Intent intent = new Intent(this, NoteActivity.class);
         intent.putExtra("Selected_Note", mNote.get(position));
+        startActivity(intent);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, NoteActivity.class);
         startActivity(intent);
 
     }
